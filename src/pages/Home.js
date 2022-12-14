03931eb4de3d1870fa9d04411f5023e2734cc5e2
Baswaraju Vuppala <bvuppala@gmail.com>
2022-12-14 09:11:03 -0800

    Initialize main

---
+++ src/pages/Home.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react'
+import FollowersColumn from '../components/FollowersColumn'
+import Card from '../components/Card'
+import MiniCard from '../components/MiniCard'
+import axios from 'axios'
+
+const Home = () => {
+  const [apps, setApps] = useState(null)
+  let descendingApps
+  //fetch all the applications to your feed
+  const fetchData = async () => {
+    const results = await axios.get('/.netlify/functions/getApps')
+    setApps(results.data)
+  
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (apps) {
+    descendingApps = apps.sort((a, b) => a.id < b.id ? 1 : -1)
+  }
+  return (
+    <>
+    {descendingApps && (
+      <div className='container'>
+        <div className="row row-cols-2">
+          {descendingApps.map((application, index) => (
+            <Card
+              key={index}
+              application={application}
+            />
+          ))}
+        </div>
+      </div>
+    )}
+    </>
+  )
+}
+
+export default Home
